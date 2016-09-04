c9bd86696ffc196240f895602f9cbece56f92a91
Jonathan Slavuter <jslavuter@andplus.com>
2016-09-03 20:04:43 -0400

    Media: Updated Resume Meta: updated favicon Portfolio: Updated promo image with fade in / out

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 $( document ).ready(function() {
     $( ".item" ).click(function() {
+
         $('body').css({"position": "fixed", "overflow": "hidden"});
 
         // Changing clicked index file to visible in drawer
         var index = $( ".item" ).index( this );
         $( "#portContainer" ).children().addClass( "inactiveImage");
         $( ".inactiveImage" ).eq( index ).removeClass( "inactiveImage");
+
+        // Fade in promo image
+        $('.logoImage').delay(250).animate({opacity: 1}, 1500);
     });
 
     // Scroll back to index upon pure-drawer close
@@ -14,5 +18,8 @@ $( document ).ready(function() {
         $('html, body').animate({
             scrollTop: $( "#portfolioSection" ).offset().top
         }, 500);
+
+        // Fade out promo image
+        $('.logoImage').delay(500).animate({opacity: 0});
     });
 });
